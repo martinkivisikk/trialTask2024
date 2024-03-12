54d0630c03389c677ebbf8db09c876409d7875b0
martinkivisikk <martinkivisikk@gmail.com>
2024-03-12 22:04:58 +0200

    Added some tests, refactoring, configurable cronjob

--- src/main/java/com/trial/trialTask/FetchService.java
+++ src/main/java/com/trial/trialTask/FetchService.java
@@ -2,6 +2,7 @@ package com.trial.trialTask;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -29,7 +31,14 @@ public class FetchService {
     private WeatherConditionRepository weatherConditionRepository;
 
     //cron = second, minute, hour, day of month, month, day of week
-    @Scheduled(cron = "0 15 * * * *")
+    //configurable in application.properties
+    @Value("${cron.expression}")
+    private String cronExpression;
+
+    /**
+     * Fetches data from ilmateenistus.ee, saves entries for Tallinn, Tartu and Pärnu.
+     */
+    @Scheduled(cron = "${cron.expression}")
     //@PostConstruct
     public void fetchDataAndSave() throws IOException, ParserConfigurationException {
         RestTemplate restTemplate = new RestTemplate();
@@ -38,35 +47,46 @@ public class FetchService {
         Collections.addAll(requiredStations, "Tallinn-Harku", "Tartu-Tõravere", "Pärnu");
 
         try {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse(new InputSource(new StringReader(xmlData)));
+            //DOM tree from xml
+            Document document = parseXmlData(xmlData);
+            Instant instant = extractTimestamp(document);
 
-            NodeList observations = document.getElementsByTagName("observations");
-            Element observation = (Element) observations.item(0);
-            long timestamp = Long.parseLong(observation.getAttribute("timestamp"));
-            Instant instant = Instant.ofEpochMilli(timestamp * 1000);
-            //System.out.println("Timestamp: " + timestamp);
+            saveWeatherData(document, requiredStations, instant);
 
-            NodeList stations = document.getElementsByTagName("station");
-
-            for (int i = 0; i < stations.getLength(); i++) {
-                Element station = (Element) stations.item(i);
-                String stationName = station.getElementsByTagName("name").item(0).getTextContent();
-                if (requiredStations.contains(stationName)) {
-                    //System.out.println(stationName);
-                    int WMOcode = Integer.parseInt(station.getElementsByTagName("wmocode").item(0).getTextContent());
-                    double airTemperature = Double.parseDouble(station.getElementsByTagName("airtemperature").item(0).getTextContent());
-                    double windSpeed = Double.parseDouble(station.getElementsByTagName("windspeed").item(0).getTextContent());
-                    String phenomenon = station.getElementsByTagName("phenomenon").item(0).getTextContent();
-                    WeatherCondition weatherCondition = new WeatherCondition(stationName, WMOcode, airTemperature, windSpeed, phenomenon, LocalDateTime.ofInstant(instant, ZoneId.of("UTC+2")));
-                    System.out.println("Saving weather data: " + weatherCondition);
-                    weatherConditionRepository.save(weatherCondition);
-                }
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    private Document parseXmlData(String xmlData) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return builder.parse(new InputSource(new StringReader(xmlData)));
+    }
+
+    private Instant extractTimestamp(Document document) {
+        NodeList observations = document.getElementsByTagName("observations");
+        Element observation = (Element) observations.item(0);
+        long timestamp = Long.parseLong(observation.getAttribute("timestamp"));
+        return Instant.ofEpochMilli(timestamp * 1000);
+    }
+
+    private void saveWeatherData(Document document, List<String> requiredStations, Instant instant) {
+        NodeList stations = document.getElementsByTagName("station");
+
+        for (int i = 0; i < stations.getLength(); i++) {
+            Element station = (Element) stations.item(i);
+            String stationName = station.getElementsByTagName("name").item(0).getTextContent();
+            if (requiredStations.contains(stationName)) {
+                //System.out.println(stationName);
+                int WMOcode = Integer.parseInt(station.getElementsByTagName("wmocode").item(0).getTextContent());
+                double airTemperature = Double.parseDouble(station.getElementsByTagName("airtemperature").item(0).getTextContent());
+                double windSpeed = Double.parseDouble(station.getElementsByTagName("windspeed").item(0).getTextContent());
+                String phenomenon = station.getElementsByTagName("phenomenon").item(0).getTextContent();
+                WeatherCondition weatherCondition = new WeatherCondition(stationName, WMOcode, airTemperature, windSpeed, phenomenon, LocalDateTime.ofInstant(instant, ZoneId.of("UTC+2")));
+                System.out.println("Saving weather data: " + weatherCondition);
+                weatherConditionRepository.save(weatherCondition);
+            }
+        }
     }
 }
--- src/test/java/com/trial/trialTask/TrialTaskApplicationTests.java
+++ src/test/java/com/trial/trialTask/TrialTaskApplicationTests.java
@@ -1,13 +1,66 @@
 package com.trial.trialTask;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TrialTaskApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private WeatherConditionRepository weatherConditionRepository;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    public void test01TallinnCar() throws Exception {
+        Mockito.when(weatherConditionRepository.findTopByWeatherStationOrderByTimeStampDesc(anyString()))
+                .thenReturn(Optional.of(new WeatherCondition("Tallinn-Harku", 1, 0.0, 21.0, "Thunder", LocalDateTime.now())));
+        mockMvc.perform(get("/get")
+                        .param("city", "Tallinn")
+                        .param("vehicleType", "Car"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
+                .andExpect(content().string("Total delivery fee is: 4.0"));
+    }
+
+    @Test
+    public void test02PärnuCar() throws Exception {
+        Mockito.when(weatherConditionRepository.findTopByWeatherStationOrderByTimeStampDesc(anyString()))
+                .thenReturn(Optional.of(new WeatherCondition("Pärnu", 1, 1.0, 5.0, "Light snowfall", LocalDateTime.now())));
+        mockMvc.perform(get("/get")
+                        .param("city", "Pärnu")
+                        .param("vehicleType", "Car"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
+                .andExpect(content().string("Total delivery fee is: 3.0"));
+    }
+
+    @Test
+    public void test03TartuExample() throws Exception {
+        Mockito.when(weatherConditionRepository.findTopByWeatherStationOrderByTimeStampDesc(anyString()))
+                .thenReturn(Optional.of(new WeatherCondition("Tartu-Tõravere", 1, -2.1, 4.7, "Light snow shower", LocalDateTime.now())));
+        mockMvc.perform(get("/get")
+                        .param("city", "Tartu")
+                        .param("vehicleType", "Bike"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"))
+                .andExpect(content().string("Total delivery fee is: 4.0"));
+    }
 }
